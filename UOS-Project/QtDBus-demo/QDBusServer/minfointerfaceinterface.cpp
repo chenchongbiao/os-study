4d9a67cf38b835eb658de152bbc47ff7b117f027
bluesky <379865549@qq.com>
2022-10-30 22:19:11 +0800

    删除

--- UOS-Project/QtDBus-demo/QDBusServer/minfointerfaceinterface.cpp
+++
@@ -1,7 +0,0 @@
-#include "minfointerfaceinterface.h"
-
-MinfoInterfaceInterface::MinfoInterfaceInterface(const QString& service, const QString& path, const QDBusConnection& connection, QObject* parent /*= nullptr*/)
-    :QDBusAbstractInterface(service,path, staticInterfaceName(), connection,parent)
-{
-
-}
