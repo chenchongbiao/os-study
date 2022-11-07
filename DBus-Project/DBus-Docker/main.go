3050eeba85b0e1681b6ef92edc69053b4dd2c85c
bluesky <379865549@qq.com>
2022-11-07 17:42:28 +0800

    修改main文件

--- DBus-Project/DBus-Docker/main.go
+++ DBus-Project/DBus-Docker/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"github.com/godbus/dbus/v5"
+	"fmt"
+
+	"./image"
+	"github.com/docker/docker/client"
+	"github.com/linuxdeepin/go-lib/dbusutil"
 )
 
-type Obj struct {
-	methods *struct {
-		GetString func() `out:"result"`
-	}
-}
+/*
+str="/usr/bin/dockerd -H tcp://localhost:2375 -H unix:///var/run/docker.sock "
+sudo sed -i "s@/usr/bin/dockerd@$str@" /usr/lib/systemd/system/docker.service
+sudo systemctl daemon-reload && systemctl restart docker
+*/
 
-func (o *Obj) GetString() (string, *dbus.Error) {
-	return "object", nil
-}
+var (
+	cli, err = client.NewClientWithOpts(client.WithAPIVersionNegotiation(), client.WithHost("tcp://localhost:2375"))
 
-func (o *Obj) GetInterfaceName() string {
-	return "p1.p2.p3"
-}
+	imageName     = "docker.io/busybox"
+	containerID   = ""
+	containerName = "busybox-test"
+)
 
 func main() {
 	service, err := dbusutil.NewSessionService()
-	obj := &Obj{}
-	err = service.Export("/p1/p2/p3", obj)
-	err = service.RequestName("p1.p2.p3")
+	mimage := image.NewImage(service)
+	err = service.Export("/com/bluesky/daemon/mdocker", mimage)
+	err = service.RequestName("com.bluesky.daemon.mdocker")
+	fmt.Println(err)
 	service.Wait()
 }
