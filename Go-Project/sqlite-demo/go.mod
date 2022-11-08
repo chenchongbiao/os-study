5b224fe22b2aa8e5dc0d70c6351d598db8ea9f6c
bluesky <379865549@qq.com>
2022-11-08 15:19:42 +0800

    添加初始化数据库操作等

--- Go-Project/sqlite-demo/go.mod
+++ Go-Project/sqlite-demo/go.mod
@@ -2,4 +2,20 @@ module sqlite-demo
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.21+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+)
--- Go-Project/sqlite-demo/main.go
+++ Go-Project/sqlite-demo/main.go
@@ -6,38 +6,60 @@ import (
 	"os"
 	"os/user"
 
+	"sqlite-demo/image"
+
+	"github.com/docker/docker/client"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	u, err := user.Current()
-	if err != nil {
-		fmt.Println(err)
-	}
+var ()
 
-	dbusDockerDir := u.HomeDir + "/.config/dbus-docker"
+func main() {
+	db := initDatabase()
+	fmt.Println("数据库初始化", db)
+	image.InitDB(db)
+	// stmt, err := db.Prepare("INSERT INTO image(username, departname, created) values(?,?,?)")
+	// printErr(err)
+	// res, err := stmt.Exec("wangshubo", "国务院", "2017-04-21")
+	// printErr(err)
+	// fmt.Println(res)
+
+	// rows, err := db.Query("SELECT * FROM image")
+	// printErr(err)
+	// var uid int
+	// var username string
+	// var department string
+	// var created time.Time
+
+	// for rows.Next() {
+	// 	err = rows.Scan(&uid, &username, &department, &created)
+	// 	printErr(err)
+	// 	fmt.Println(uid)
+	// 	fmt.Println(username)
+	// 	fmt.Println(department)
+	// 	fmt.Println(created)
+	// }
+}
+func initDatabase() *sql.DB {
+	Host := "tcp://localhost:2375"
+	_, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation(), client.WithHost(Host))
+	printErr(err)
+
+	u, err := user.Current()
+	printErr(err)
+
+	dbusDockerDir := u.HomeDir + "/.config/dbus-docker/data"
 
 	if !isExist(dbusDockerDir) {
 		err = os.Mkdir(dbusDockerDir, 0700)
-		if err != nil {
-			fmt.Println(err)
-		}
+		printErr(err)
 	}
 
 	//打开数据库，如果不存在，则创建
 	dbPath := dbusDockerDir + "/db.sqlite"
 	db, err := sql.Open("sqlite3", dbPath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	//创建表
-	sql_table := `
-	CREATE TABLE IF NOT EXISTS image(
-		uid INTEGER PRIMARY KEY AUTOINCREMENT,
-		username VARCHAR(64) NULL,
-		departname VARCHAR(64) NULL,
-		created DATE NULL);`
-	db.Exec(sql_table)
+	printErr(err)
+	return db
 }
 
 func isExist(path string) bool {
@@ -49,8 +71,13 @@ func isExist(path string) bool {
 		if os.IsNotExist(err) {
 			return false
 		}
-		fmt.Println(err)
 		return false
 	}
 	return true
+}
+
+func printErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
