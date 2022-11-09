1d1d5df4a3994fac93d9d1fff087762b0b6cc848
bluesky <379865549@qq.com>
2022-11-09 11:25:33 +0800

    修改Size变量的类型

--- Go-Project/sqlite-demo/image/ImageModel.go
+++ Go-Project/sqlite-demo/image/ImageModel.go
@@ -1,8 +1,8 @@
 package image
 
 type ImageModel struct {
-	ImgId      string  // 镜像id
-	Tags       string  // 镜像标签
-	Size       float32 // 镜像尺寸
-	CreateTime string  // 镜像创建时间
+	ImgId      string // 镜像id
+	Tags       string // 镜像标签
+	Size       string // 镜像尺寸
+	CreateTime string // 镜像创建时间
 }
