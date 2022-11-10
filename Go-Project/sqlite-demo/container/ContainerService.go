d892223749e76e63f7c80269c265ccc595665c39
bluesky <379865549@qq.com>
2022-11-10 09:13:15 +0800

    添加GetContainerList函数

--- Go-Project/sqlite-demo/container/ContainerService.go
+++ Go-Project/sqlite-demo/container/ContainerService.go
@@ -1,8 +1,11 @@
 package container
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 
+	"github.com/docker/docker/api/types"
 	"golang.org/x/vuln/client"
 )
 
@@ -18,4 +21,31 @@ func InitService(cli *client.Client, db *sql.DB) *ContainerMapper {
 		containerMapper: containerMapper,
 	}
 	return &containerService
+}
+
+func (c *ContainerService) GetContainerList() (result string, err error) {
+	ctx := context.Background()
+	containers, err := c.cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		fmt.Println("GetContainerList", err)
+		result = "获取容器列表失败"
+		return
+	}
+	for _, image := range containers {
+		// var img_id string
+		// var tags string
+		// var size string
+		// var create_time string
+		// result := i.imgMapper.SelectImageByImgId(image.ID[7:]) // 根据镜像ID查找是否数据已有数据
+		// if !result {
+		// 	img_id = image.ID[7:]
+		// 	tags = arrayToString(image.RepoTags)
+		// 	size = formatImageSize(image.Size)
+		// 	create_time = fotmatDate(image.Created)
+		// 	tb := NewImageItem(img_id, tags, size, create_time)
+		// 	i.imgMapper.Insert(tb)
+		// }
+	}
+	result = "获取镜像列表成功"
+	return result, err
 }
