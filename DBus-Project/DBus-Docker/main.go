7ab1139b11842cba751da9ef471521363dea04da
bluesky <379865549@qq.com>
2022-11-12 18:35:47 +0800

    不使用root用户也可以使用docker

--- DBus-Project/DBus-Docker/main.go
+++ DBus-Project/DBus-Docker/main.go
@@ -10,9 +10,13 @@ import (
 )
 
 /*
+暴露端口
 str="/usr/bin/dockerd -H tcp://localhost:2375 -H unix:///var/run/docker.sock "
 sudo sed -i "s@/usr/bin/dockerd@$str@" /usr/lib/systemd/system/docker.service
 sudo systemctl daemon-reload && systemctl restart docker
+
+将当前用户加入到docker用户组
+sudo usermod -aG docker ${USER} & sudo systemctl restart docker
 */
 
 var (
