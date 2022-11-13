bd42e5510f0671540cdd1d81dfae23aeefb7b86f
bluesky <379865549@qq.com>
2022-11-13 21:43:06 +0800

    添加数据库初始化函数

--- DBus-Project/DBus-Docker/config/config.go
+++ DBus-Project/DBus-Docker/config/config.go
@@ -1,11 +1,37 @@
 package config
 
-import "os"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"os/user"
+)
 
 const (
 	Success_code = 1
 	Fail_code    = 0
 )
+
+func InitDatabase() (*sql.DB, error) {
+	u, err := user.Current()
+	if err != nil {
+		fmt.Println("获取当前用户失败", err)
+	}
+
+	dbusDockerDir := u.HomeDir + "/.config/dbus-docker/data"
+
+	if !IsExist(dbusDockerDir) {
+		err = os.MkdirAll(dbusDockerDir, 0700)
+		if err != nil {
+			fmt.Println("文件创建失败", err)
+		}
+	}
+
+	//打开数据库，如果不存在，则创建
+	dbPath := dbusDockerDir + "/db.sqlite"
+	db, err := sql.Open("sqlite3", dbPath)
+	return db, err
+}
 
 func IsExist(path string) bool {
 	_, err := os.Stat(path)
