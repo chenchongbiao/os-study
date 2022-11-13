e1d3c27a0c81017f0876322226c586019b8f4556
bluesky <379865549@qq.com>
2022-11-13 21:40:30 +0800

    添加config配置包

--- DBus-Project/DBus-Docker/config/config.go
+++ DBus-Project/DBus-Docker/config/config.go
@@ -1,3 +1,22 @@
 package config
 
-const ()
+import "os"
+
+const (
+	Success_code = 1
+	Fail_code    = 0
+)
+
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
+}
