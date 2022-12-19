215ef1c142d8255c4760c59b8f2e620140f56fe5
bluesky <379865549@qq.com>
2022-12-19 19:04:40 +0800

    添加config.keyLists和config.value的函数调用

--- Qt-Project/dconfig-simple-example/main.cpp
+++ Qt-Project/dconfig-simple-example/main.cpp
@@ -15,11 +15,18 @@ int main(int argc, char *argv[])
     DConfig config(fileName);
     // QScopedPointer<DConfig> config(DConfig::create(appId, fileName));
     // 判断是否有效
-    // if (!config.isValid()) {
-    //     qWarning() << QString("DConfig无效, name:[%1], subpath[%2].").
-    //                   arg(config.name(), config.subpath());
-    //     return 0;
-    // }
+    if (!config.isValid()) {
+        qWarning() << QString("DConfig无效, name:[%1], subpath[%2].").
+                      arg(config.name(), config.subpath());
+        return 0;
+    }
+
+    // 获取所有配置项的key
+    qDebug() << "所有的键值" << config.keyList();
+
+    // 获取指定配置项的值
+    qDebug() << "key item's value:" << config.value("canExit").toBool();
+
     
     // 异常处理，调用QCoreApplication::exit，使DConfig正常析构。
     std::signal(SIGINT, &QCoreApplication::exit);
