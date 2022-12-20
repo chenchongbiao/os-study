765a604d9e2a5a117d9bea08232d6bbafc907b10
bluesky <379865549@qq.com>
2022-12-20 11:01:15 +0800

    修改main的代码

--- Qt-Project/dconfig-simple-example/main.cpp
+++ Qt-Project/dconfig-simple-example/main.cpp
@@ -1,6 +1,5 @@
 #include <DConfig>
 #include <QDebug>
-#include <csignal>
 #include <QCoreApplication>
 
 DCORE_USE_NAMESPACE
@@ -8,72 +7,49 @@ DCORE_USE_NAMESPACE
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    const QString fileName = "example";
-    {
-        // 构造DConfig
-        DConfig config(fileName);
+    
+    // 构造DConfig，元数据文件名example，与元数据安装目录的/usr/share/dsg/configs/APPID名(可执行文件名)对应目录/example.json，
+    DConfig config("example");
 
-        // 判断是否有效
-        if (!config.isValid()) {
-            qWarning() << QString("DConfig无效, name:[%1], subpath[%2].").
-                        arg(config.name(), config.subpath());
-            return 0;
-        }
-
-        // 获取所有配置项的key
-        qDebug() << "所有的所有配置项的key:" << config.keyList();
-
-        // 返回配置策略后端名称 "DBusBackend"
-        qDebug() << "配置策略后端名称:" << config.backendName();
-
-        // 获取指定配置项的值
-        qDebug() << "canExit对应的值:" << config.value("canExit").toBool();
+    // 判断是否有效
+    if (!config.isValid()) {
+        qWarning() << QString("DConfig无效, name:[%1].").
+                      arg(config.name());
+        return 0;
     }
-    {
-        // 构造DConfig，subpath="/a"
-        DConfig config(fileName,"/a");
 
-        // 判断是否有效
-        if (!config.isValid()) {
-            qWarning() << QString("DConfig无效, name:[%1], subpath[%2].").
-                        arg(config.name(), config.subpath());
-            return 0;
+    // 获取所有配置项的key
+    qDebug() << "所有的所有配置项的key:" << config.keyList();
+
+    // 获取指定配置项的值，配置项可以是字符串，数组，map容器，布尔值，整型，详见example.json
+    qDebug() << "canExit对应的值:" << config.value("canExit").toBool();
+
+    QVariantMap map;
+    for (int i = 0; i < 1; i++) {
+        QVariantMap nestItem;
+        for (int j = 0; j < 1; j++) {
+            nestItem[QString::number(j)] = QString::number(j);
         }
-
-        // 获取指定配置项的值
-        qDebug() << "subpath下key3对应的值:" << config.value("key3").toString();
+        map[QString::number(i)] = nestItem;
     }
-    {
-        QVariantMap map;
-        for (int i = 0; i < 1; i++) {
-            QVariantMap nestItem;
-            for (int j = 0; j < 1; j++) {
-                nestItem[QString::number(j)] = QString::number(j);
-            }
-            map[QString::number(i)] = nestItem;
-        }
-        // 构造DConfig
-        DConfig config(fileName);
 
-        // 先打印一遍原来的值
-        qDebug() <<  "第一次打印map对应的值:" << config.value("map", map);
+    QScopedPointer<DConfig> heapConfig;
+    heapConfig.reset(new DConfig("example"));
 
-        // 设置map对应的值为
-        config.setValue("map", map);
-
-        // 获取指定map对应的值
-        qDebug() <<  "第二次打印map对应的值:" << config.value("map", map);
-
-        // 重置map的值
-        config.reset("map");
-        // 获取指定map对应的值
-        qDebug() <<  "第三次打印map对应的值:" << config.value("map", map);
-        
+    if (!heapConfig->isValid()) {
+        qWarning() << QString("DConfig无效, name:[%1].").
+                      arg(heapConfig->name());
+        return 0;
     }
-    // 异常处理，调用QCoreApplication::exit，使DConfig正常析构。
-    std::signal(SIGINT, &QCoreApplication::exit);
-    std::signal(SIGABRT, &QCoreApplication::exit);
-    std::signal(SIGTERM, &QCoreApplication::exit);
-    std::signal(SIGKILL, &QCoreApplication::exit) ;
+
+    // 监听值改变的信号
+    const bool &oldValue = heapConfig->value("canExit").toBool();
+    QObject::connect(heapConfig.get(), &DConfig::valueChanged, [oldValue,&heapConfig](const QString &key){
+        qDebug() << "canExit原来的值:" << oldValue << ", canExit新的值:" << heapConfig->value(key).toBool();
+    });
+
+    // 重置canExit的值
+    heapConfig->setValue("canExit", !oldValue);
+
     return a.exec();
 }
