1c5eda9e8cd27d8c8d3b321762aa82d6c2c37111
bluesky <379865549@qq.com>
2023-01-01 13:42:02 +0800

    添加ddciicon的例子

--- Qt-Project/ddciicon-example/main.cpp
+++ Qt-Project/ddciicon-example/main.cpp
@@ -1,18 +1,40 @@
 #include <DApplication>
 #include <DWidgetUtil>
-#include <DMainWindow>
 #include <DIconButton>
+#include <DDciIcon>
+#include <QFile>
+#include <QtDebug>
+#include <QPixmap>
+#include <QLineEdit>
+#include <QFormLayout>
+#include <DIconButton>
+#include <QWidget>
+#include <QLabel>
 
 DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     DApplication a(argc, argv);
     
-    DMainWindow w;
-    w.show();
+    QWidget w;  // 初始化主窗口
+    QFormLayout* layout = new QFormLayout(&w);
 
+    QFile file(":/dsg/built-in-icons/select_indicator.dci");
+    file.open(QIODevice::ReadOnly);
+    DDciIcon dci(file.fileName());
+
+    auto icon = dci.matchIcon(0, DDciIcon::Light, DDciIcon::Hover);
+    qDebug() << "DCI图标的实际大小" << dci.actualSize(icon);
+
+    layout->addRow("icon button", new DIconButton(dci));
+
+    QLabel *label = new QLabel();
+    label->setPixmap(dci.pixmap(1.0, 0, icon));
+    layout->addRow("icon", label);
+
+
+    w.show();
     Dtk::Widget::moveToCenter(&w);
     return a.exec();
 }
